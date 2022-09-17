47263c5b5a4c931f5b91f7333cecee390421a33f
xushun1221 <xushun1221@outlook.com>
2022-09-17 15:09:02 +0800

    完成InetAddress封装

---
+++ InetAddress.hh
@@ -0,0 +1,28 @@
+#ifndef   __INETADDRESS_HH_
+#define   __INETADDRESS_HH_
+
+#include <arpa/inet.h>
+#include <string>
+
+/* socket地址类型封装 */
+class InetAddress {
+public:
+    /* 使用ip+port构造socket地址 */
+    explicit InetAddress(uint16_t port, std::string ip = "127.0.0.1");
+    /* 使用sockaddr_in直接构造socket地址 */
+    explicit InetAddress(const sockaddr_in& addr) : addr_(addr) {}
+    /* 获取ip字符串 */
+    std::string toIp() const;
+    /* 获取ip+port字符串 */
+    std::string toIpPort() const;
+    /* 获取port */
+    uint16_t toPort() const;
+    /* 获得内部的sockaddr_in */
+    const sockaddr_in* getSockAddr() const { return &addr_; }
+private:
+    /* socket地址类型 仅支持ipv4 */
+    sockaddr_in addr_;
+};
+
+
+#endif // __INETADDRESS_HH_
