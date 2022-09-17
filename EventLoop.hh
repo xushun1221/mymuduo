f202c25e4bd6327da4c88d0621d7f9e7027253ee
xushun1221 <xushun1221@outlook.com>
2022-09-17 15:58:53 +0800

    添加TcpServer和EventLoop空文件

---
+++ EventLoop.hh
@@ -0,0 +1,10 @@
+#ifndef   __EVENTLOOP_HH_
+#define   __EVENTLOOP_HH_
+
+/* EventLoop事件循环类 主要包含Channel和Poller(epoll)两大模块 */
+class EventLoop {
+
+};
+
+
+#endif // __EVENTLOOP_HH_
---
+++ TcpServer.hh
@@ -0,0 +1,10 @@
+#ifndef   __TCPSERVER_HH_
+#define   __TCPSERVER_HH_
+
+/* TcpServer服务器类 服务器编程的入口类 */
+class TcpServer {
+
+};
+
+
+#endif // __TCPSERVER_HH_
