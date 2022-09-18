854b195e9e504e524bff85d85de9252507d87e16
xushun1221 <xushun1221@outlook.com>
2022-09-18 21:36:59 +0800

    添加index_的注释

--- Channel.hh
+++ Channel.hh
@@ -76,7 +76,7 @@ private:
     const int fd_;      /* fd poller监听的对象 */
     int events_;        /* 注册fd感兴趣的事件 */
     int revents_;       /* poller返回的具体发生的事件 */
-    int index_; /* 索引号 */
+    int index_; /* 表示该Channel在Poller中的状态 -1 1 2 */
 
     std::weak_ptr<void> tie_;
     bool tied_;
