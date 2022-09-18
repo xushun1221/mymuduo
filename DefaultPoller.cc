8cbbe279f9a19ea01563b5e3dbee62add68edafd
xushun1221 <xushun1221@outlook.com>
2022-09-18 10:22:49 +0800

    实现Poller抽象类 添加newDefalutPoller接口的源文件(尚未实现)

---
+++ DefaultPoller.cc
@@ -0,0 +1,13 @@
+#include "Poller.hh"
+// add code 
+#include <stdlib.h>
+
+/* EventLoop可以通过该接口获得默认IO复用的具体实现 */
+Poller* Poller::newDefaultPoller(EventLoop* loop) {
+    /* 默认提供epoll的实例 如果环境变量中有MUDUO_USE_POLL则返回poll的实例 */
+    if (::getenv("MUDUO_USE_POLL")) {
+        return nullptr; // add code 生成poll的实例
+    } else {
+        return nullptr; // add code 生成epoll的实例
+    }
+}
---
+++ Poller.cc
@@ -0,0 +1,23 @@
+#include "Poller.hh"
+#include "Channel.hh"
+
+/* channel是否在当前Poller中 */
+bool Poller::hasChannel(Channel* channel) const {
+    ChannelMap::const_iterator it = channels_.find(channel->fd());
+    return it != channels_.end() && it->second == channel;
+}
+
+
+
+/* 
+    如果我们在这里实现newDefaultPoller
+    需要返回某个实例化的EpollPoller对象
+    那势必需要在这里包含 派生类EpollPoller的头文件
+    但是在基类的实现中包含派生类的头文件是不合适的
+    所以我们不在这里实现newDeafultPoller
+    使用一个独立的源文件DefaultPoller.cc来实现
+*/
+
+// #include "EpollPoller.hh"
+// Poller* Poller::newDefaultPoller(EventLoop* loop) {
+// }
