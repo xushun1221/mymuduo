baaccdaac26adc2d29b3ccd3eee6ad963d0dd3f4
xushun1221 <xushun1221@outlook.com>
2022-09-25 16:08:42 +0800

    添加高水位回调类型

--- Callbacks.hh
+++ Callbacks.hh
@@ -9,9 +9,11 @@ class TcpConnection;
 class Timestamp;
 
 using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+
 using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
 using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
 using WriteCompleteCallback = std::function<void(const TcpConnectionPtr&)>;
+using HighWaterMarkCallback = std::function<void(const TcpConnectionPtr&, size_t)>;
 
 using MessageCallback = std::function<void(const TcpConnectionPtr& conn, Buffer* buffer, Timestamp receiveTime)>;
 
